9ce9c764b14ae448c27d886074573dcb98c5a43f
Andrés Coll Prediger <andrescollsn@gmail.com>
2022-09-15 00:35:24 -0300

    1er commit

---
+++ Clase_9 --Ejercicio_2/main.js
@@ -0,0 +1,10 @@
+var pais = "Argentina";
+var continente = "Sudamerica";
+var antiguedad = "2022";
+var pais_y_continente = pais + " " + continente;
+
+pais = "Francia";
+continente = "Europa";
+
+console.log(pais, continente, antiguedad);
+alert (pais_y_continente);
